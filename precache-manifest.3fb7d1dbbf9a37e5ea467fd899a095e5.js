0837bf5049b7b0410871a346afcbd593b265a3e9
Lauro Pereira <lauropera01@gmail.com>
2022-08-13 15:11:27 -0300

    Updates

---
+++ precache-manifest.3fb7d1dbbf9a37e5ea467fd899a095e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c54c40e14cafb53c88f26fa02222b25",
+    "url": "/wallet/index.html"
+  },
+  {
+    "revision": "171a30b65679183b12ee",
+    "url": "/wallet/static/css/main.960d4c9e.chunk.css"
+  },
+  {
+    "revision": "b6a89611f2feae194220",
+    "url": "/wallet/static/js/2.6797fd46.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/wallet/static/js/2.6797fd46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "171a30b65679183b12ee",
+    "url": "/wallet/static/js/main.8913a178.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/wallet/static/js/main.8913a178.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fb4cea7ccc09b9511f8",
+    "url": "/wallet/static/js/runtime-main.8f1525cf.js"
+  }
+]);
